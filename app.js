1a9a582aeb174523cb3e16dc694a9871ea2f6739
oviciana <vicfer.oscar@gmail.com>
2017-03-10 12:31:48 +0100

    changes for push to heroku

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ const UserOrders = require('./models/UserOrders.js')
 const PORT = 3010
 const app = express()
 
-mongoose.Promise = global.Promise
+// mongoose.Promise = global.Promise
 
-app.use( express.static( __dirname + '/public' ) )
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+ app.use( express.static( __dirname + '/public' ) )
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
-app.set( 'view engine', 'pug' )
+// app.set( 'view engine', 'pug' )
 
-const urlDB = 'mongodb://localhost:27017/combarrio'
-mongoose.connect(urlDB)
+// const urlDB = 'mongodb://localhost:27017/combarrio'
+// mongoose.connect(urlDB)
 
-app.get('/prova', (req,res) => {
+// app.get('/prova', (req,res) => {
 
-	res.render('C:/Skylab/ProjectComBarri/views/layout.html')
-})
+// 	res.render('C:/Skylab/ProjectComBarri/views/layout.html')
+// })
 
 
-app.listen(PORT,() =>	console.log(`listening on port ${PORT}...`)) 
+// app.listen(PORT,() =>	console.log(`listening on port ${PORT}...`)) 
