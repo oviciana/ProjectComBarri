4b34d1731c519149090e2f8d5d9809e15aa1e813
oviciana <vicfer.oscar@gmail.com>
2017-03-24 13:24:01 +0100

    review final demo

--- client/js/appConstants.js
+++ client/js/appConstants.js
@@ -1,7 +1,7 @@
 (function () {
   angular.module('comBarrioApp')
     .constant('constApp', {
-//      urlMain : 'https://comer-barri17.herokuapp.com'
-      urlMain : 'http://localhost:3010'
+      urlMain : 'https://comer-barri17.herokuapp.com'
+//      urlMain : 'http://localhost:3010'
     })
 })()
